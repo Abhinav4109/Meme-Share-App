f12f2c4f331283d0b9c7d19e38e61e840adfaebd
Abhinav Singh <abhinavsinghabhi56@gmail.com>
2022-07-30 09:06:14 +0530

    Showing Memes From Reddit

---
+++ app/src/main/java/com/example/meme/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.example.meme
+
+import android.content.Intent
+import android.graphics.drawable.Drawable
+import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+
+class MainActivity : AppCompatActivity() {
+    var memeImage: String? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        loadMeme()
+    }
+
+    private fun loadMeme() {
+        findViewById<ProgressBar>(R.id.progressBar).visibility = View.VISIBLE
+
+        val url = "https://meme-api.herokuapp.com/gimme"
+
+        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null, { response ->
+            memeImage = response.getString("url")
+            Glide.with(this).load(memeImage).listener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    findViewById<ProgressBar>(R.id.progressBar).visibility =
+                        View.GONE
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    findViewById<ProgressBar>(R.id.progressBar).visibility =
+                        View.GONE
+                    return false
+                }
+            }
+            ).into(findViewById(R.id.memeImageView))
+        },
+            { Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show() }
+        )
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
+    }
+
+    fun shareMeme(view: View) {
+        val i = Intent(Intent.ACTION_SEND)
+        i.type = "text/plain"
+        i.putExtra(Intent.EXTRA_TEXT, "Hi, checkout this meme $memeImage")
+        startActivity(Intent.createChooser(i, "Share this meme with"))
+    }
+    fun nextMeme(view: View) {
+        loadMeme()
+    }
+}
